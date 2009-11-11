576f2a878c19eeb36480b4a730c69064e993e264
laihj <hamainter@gmail.com>
2009-11-11 22:09:08 +0800

    保存单个文件，文件夹还不成

--- photoTool/photoResize.py
+++ photoTool/photoResize.py
@@ -1,4 +1,37 @@
 import Image
+import sys
+import os
 
-def blogSize(self,image)
-    pass
+absFilePath = os.path.abspath('.')
+
+def blogSize(image,filepath):
+    image = image.resize((600,600*image.size[1]/image.size[0]),Image.NEAREST)
+    if not os.path.exists(absFilePath + '/blog'):
+        os.mkdir(absFilePath + '/blog')
+    print filepath
+    image.save(absFilePath + '/blog/' + filepath)
+
+def main(argv):
+    for arg in argv:
+        print arg
+    if argv[1] == '-f':
+        print 'afile'
+        try:
+            im = Image.open(absFilePath + '/' + argv[2])
+            blogSize(im,argv[2])
+        except:
+            pass
+    else:
+        #for root,dirs,files in os.walk(absFilePath.join(argv[1])):
+        for name in ['IMG_0896.JPG','IMG_0900.JPG']:
+            try:
+                im = Image.open(absFIlePath + '/' + name)
+                blogSize(im,name)
+                print(absFilePath + '/' + name)
+            except:
+                continue
+
+                     
+            
+if __name__ == '__main__':
+    main(sys.argv)
